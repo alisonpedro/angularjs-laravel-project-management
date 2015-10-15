4e1163edd3387eb91a9ad8fdb441a770cff99f3e
Alison Pedro <alisondp@hotmail.com>
2015-10-15 16:33:52 -0300

    Client Service

---
+++ laravel-app/app/Services/ClientService.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SUPORTE
+ * Date: 15/10/2015
+ * Time: 16:03
+ */
+
+namespace CodeProject\Services;
+
+
+use CodeProject\Repositories\ClientRepository;
+use CodeProject\Validator\ClientValidator;
+use Prettus\Validator\Exceptions\ValidatorException;
+
+class ClientService
+{
+    /**
+     * @var ClientRepository
+     */
+
+    protected $repository;
+    /**
+     * @var ClientValidator
+     */
+    protected $validator;
+
+    public function __construct(ClientRepository $repository, ClientValidator $validator)
+    {
+        $this->repository = $repository;
+        $this->validator = $validator;
+    }
+
+    public function create(array $data)
+    {
+        try {
+            $this->validator->with($data)->passesOrFail();
+            return $this->repository->create($data);
+        }catch(ValidatorException $e){
+            return [
+                'error' => true,
+                'message' => $e->getMessageBag()
+            ];
+
+        }
+
+    }
+
+    public function update(array $data, $id)
+    {
+        return $this->repository->update($data, $id);
+    }
+}
---
+++ laravel-app/app/Validators/ClientValidator.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SUPORTE
+ * Date: 15/10/2015
+ * Time: 16:18
+ */
+
+namespace CodeProject\Validator;
+
+
+use Prettus\Validator\LaravelValidator;
+
+class ClientValidator extends LaravelValidator
+{
+    protected $rules = [
+        'name' => 'required|max:255',
+        'responsible' => 'required|max:255',
+        'email' => 'required|email',
+        'phone' => 'required',
+        'address' => 'required'
+    ];
+
+}
