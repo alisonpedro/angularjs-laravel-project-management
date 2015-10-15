ffa456d356c56797d0a1fd66cbf0766fa3fd0679
Alison Pedro <alisondp@hotmail.com>
2015-10-15 17:08:40 -0300

    Project

--- laravel-app/database/seeds/ProjectTableSeeder.php
+++ laravel-app/database/seeds/ProjectTableSeeder.php
@@ -11,7 +11,7 @@ class ClientTableSeeder extends Seeder
      */
     public function run()
     {
-        //\CodeProject\Entities\Client::truncate();
-        factory(\CodeProject\Entities\Client::class, 10)->create();
+        //\CodeProject\Entities\Project::truncate();
+        factory(\CodeProject\Entities\Project::class, 10)->create();
     }
 }
