30214948ddfe058305587ccdd13b603e48501b5a
Alison Pedro <alisondp@hotmail.com>
2015-10-16 14:46:53 -0300

    Routes Refactor

--- laravel-app/app/Http/routes.php
+++ laravel-app/app/Http/routes.php
@@ -19,21 +19,25 @@ Route::post('oauth/access_token', function(){
     return Response::json(Authorizer::issueAccessToken());
 });
 
-Route::get('client', ['middleware'=>'oauth','uses'=>'ClientController@index']);
+
+Route::group(['middleware'=>'oauth'], function(){
+    //Client
+    Route::resource('client','ClientController', ['except'=>['create','edit']]);
+
+    Route::group(['prefix'=>'project'], function(){
+        //Project
+        Route::resource('','ProjectController', ['except'=>['create','edit']]);
+        //ProjectNote
+        Route::get('{id}/note', 'ProjectNoteController@index');
+        Route::post('{id}/note/', 'ProjectNoteController@store');
+        Route::get('{id}/note/{noteId}', 'ProjectNoteController@show');
+        Route::put('{id}/note/{noteId}', 'ProjectNoteController@update');
+        Route::delete('{id}/note/{noteId}', 'ProjectNoteController@destroy');
+
+    });
+
+});
+
+//Route::get('client', ['middleware'=>'oauth','uses'=>'ClientController@index']);
 
 
-//Route::get('client', 'ClientController@index');
-Route::post('client', 'ClientController@store');
-Route::get('client/{id}', 'ClientController@show');
-Route::delete('client/{id}', 'ClientController@destroy');
-
-Route::get('project/{id}/note', 'ProjectNoteController@index');
-Route::post('project/{id}/note/', 'ProjectNoteController@store');
-Route::get('project/{id}/note/{noteId}', 'ProjectNoteController@show');
-Route::put('project/{id}/note/{noteId}', 'ProjectNoteController@update');
-Route::delete('project/{id}/note/{noteId}', 'ProjectNoteController@destroy');
-
-Route::get('project', 'ProjectController@index');
-Route::post('project', 'ProjectController@store');
-Route::get('project/{id}', 'ProjectController@show');
-Route::delete('project/{id}', 'ProjectController@destroy');
