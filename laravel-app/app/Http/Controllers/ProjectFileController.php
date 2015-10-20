cccb17239916eb7cc5c6df7f145a13141645252b
Alison Pedro <alisondp@hotmail.com>
2015-10-20 12:41:44 -0200

    file upload

--- laravel-app/app/Http/Controllers/ProjectFileController.php
+++ laravel-app/app/Http/Controllers/ProjectFileController.php
@@ -53,7 +53,13 @@ class ProjectFileController extends Controller
         $file = $request->file('file');
         $extension = $file->getClientOriginalExtension();
 
-        Storage::put($request->name . '.' . $extension, File::get($file));
+        $data['file'] = $file;
+        $data['extension'] = $extension;
+        $data['name'] = $request->name;
+        $data['project_id'] = $request->project_id;
+        $data['description'] = $request->description;
+
+        $this->service->createFile($data);
     }
 
     /**
--- laravel-app/app/Services/ProjectService.php
+++ laravel-app/app/Services/ProjectService.php
@@ -12,7 +12,9 @@ namespace CodeProject\Services;
 use CodeProject\Repositories\ClientRepository;
 use CodeProject\Repositories\ProjectRepository;
 use CodeProject\Validators\ProjectValidator;
+use Illuminate\Contracts\Filesystem\Factory as Storage;
 use Prettus\Validator\Exceptions\ValidatorException;
+use Illuminate\Filesystem\Filesystem;
 
 class ProjectService
 {
@@ -25,11 +27,21 @@ class ProjectService
      * @var ClientValidator
      */
     protected $validator;
+    /**
+     * @var Filesystem
+     */
+    private $filesystem;
+    /**
+     * @var Storage
+     */
+    private $storage;
 
-    public function __construct(ProjectRepository $repository, ProjectValidator $validator)
+    public function __construct(ProjectRepository $repository, ProjectValidator $validator, Filesystem $filesystem, Storage $storage)
     {
         $this->repository = $repository;
         $this->validator = $validator;
+        $this->filesystem = $filesystem;
+        $this->storage = $storage;
     }
 
     public function create(array $data)
@@ -50,5 +62,13 @@ class ProjectService
     public function update(array $data, $id)
     {
         return $this->repository->update($data, $id);
+    }
+
+    public function createFile(array $data)
+    {
+        $project = $this->repository->skipPresenter()->find($data['project_id']);
+        $projectFile = $project->files()->create($data);
+
+        $this->storage->put($projectFile->id . '.' . $data['extension'], $this->filesystem->get($data['file']));
     }
 }
